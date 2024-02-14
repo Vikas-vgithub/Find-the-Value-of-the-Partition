1c78ad65e8f9dd7875d8b445e12a431a7b408563
Vikas Verma <110339905+Vikas-vgithub@users.noreply.github.com>
2024-02-14 17:43:56 +0530

    Create Find-the-Value-of-the-Partition.java

---
+++ Find-the-Value-of-the-Partition.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findValueOfPartition(int[] nums) {
+        Arrays.sort(nums);
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0; i<nums.length-1; i++){
+            ans = Math.min(ans, nums[i+1]-nums[i]);
+            System.out.println(ans);
+        }
+
+        return ans;
+
+    }
+}
